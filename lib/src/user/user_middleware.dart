8b8e934b09dfd3ad7ecce7f86fabc1b0219ef984
swimmingkiim <swimmingkiim@gmail.com>
2022-11-01 19:37:33 +0900

    Update to print user name if exist

--- lib/src/user/user_middleware.dart
+++ lib/src/user/user_middleware.dart
@@ -1,9 +1,13 @@
+import 'package:collection/collection.dart';
 import 'package:shelf/shelf.dart';
 
 class UserMiddleware {
   Middleware printUserName() {
     return createMiddleware(requestHandler: (Request request) {
       print('FROM_USER:${request.requestedUri.path}');
+      if (request.url.pathSegments.firstOrNull != null) {
+        print('USER_NAME:${request.url.pathSegments.first}');
+      }
       return;
     });
   }
