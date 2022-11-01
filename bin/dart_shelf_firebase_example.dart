6787578d4e4dbadfa2d752818c3cc74acbdbed41
swimmingkiim <swimmingkiim@gmail.com>
2022-11-01 18:52:27 +0900

    Split into dev and prod

--- bin/dart_shelf_firebase_example.dart
+++ bin/dart_shelf_firebase_example.dart
@@ -1,12 +1,10 @@
-import 'dart:io';
-
-import 'package:shelf/shelf_io.dart' as shelf_io;
-import 'package:dart_shelf_firebase_example/dart_shelf_firebase_example.dart';
+import 'dev.dart';
+import 'prod.dart';
 
 void main(List<String> arguments) async {
-  final app = AppModule();
-  final server =
-      await shelf_io.serve(app.router, InternetAddress.loopbackIPv4, 8080);
-
-  print('Serving at http://${server.address.host}:${server.port}');
+  if (arguments[0] == 'dev') {
+    mainDev();
+  } else {
+    await mainProd();
+  }
 }
---
+++ bin/dev.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+import 'package:shelf/shelf_io.dart' as shelf_io;
+import 'package:dart_shelf_firebase_example/dart_shelf_firebase_example.dart';
+
+void mainDev() async {
+  final app = AppModule();
+  final server =
+      await shelf_io.serve(app.router, InternetAddress.loopbackIPv4, 8080);
+
+  print('Serving at http://${server.address.host}:${server.port}');
+}
