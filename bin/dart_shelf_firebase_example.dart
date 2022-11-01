0acebfc8ac0298575184d8a118be560b59dbb430
swimmingkiim <swimmingkiim@gmail.com>
2022-11-01 18:03:29 +0900

    Initialize project

---
+++ bin/dart_shelf_firebase_example.dart
@@ -0,0 +1,5 @@
+import 'package:dart_shelf_firebase_example/dart_shelf_firebase_example.dart' as dart_shelf_firebase_example;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_shelf_firebase_example.calculate()}!');
+}
---
+++ test/dart_shelf_firebase_example_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_shelf_firebase_example/dart_shelf_firebase_example.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
